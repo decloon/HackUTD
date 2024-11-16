a874af343aee8260119995b17a550773cf65f3fa
UbaidM0 <usmohammed0@gmail.com>
2024-11-16 17:37:33 -0600

    sql schema

---
+++ create_table.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE property_metrics (
+    id SERIAL PRIMARY KEY,
+    month VARCHAR(50),
+    size_sqft INTEGER,
+    total_expense NUMERIC,
+    electricity_bill NUMERIC,
+    electricity_usage NUMERIC,
+    water_bill NUMERIC,
+    water_usage NUMERIC,
+    waste_produced NUMERIC,
+    percent_waste_recycled NUMERIC,
+    hvac_expenses NUMERIC,
+    lighting_expenses NUMERIC,
+    ghg_emissions NUMERIC
+);
