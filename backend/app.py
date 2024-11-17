674100657e6202e9644e82ae0e2255f0efed4b70
UbaidM0 <usmohammed0@gmail.com>
2024-11-17 04:29:22 -0600

    api to visualize csv and sentiment

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ import pandas as pd
 from get_insights import process_data
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})  # Allow requests from React app) 
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'csv'}
@@ -16,7 +17,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST', 'GET'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
@@ -25,11 +26,10 @@ def upload_file():
         return jsonify({'error': 'No selected file'}), 400
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
+        file.save(filename)
         
         # Process the uploaded file
-        result = process_data(filepath)
+        result = process_data(filename)
         
         return jsonify(result)
     else:
@@ -54,7 +54,7 @@ def handle_post():
     return jsonify(info)
       
 
-CORS(app)
+# CORS(app)
 if __name__ == '__main__':
-        app.run(port=5000, debug=True)
+    app.run(port=5173, debug=True)
 
